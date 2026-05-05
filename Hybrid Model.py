cd9e0f16c2a804ec56900ad97b28ee470016efc6
ziadmalkoun-star <ziad.malkoun@corsicasole.com>
2026-05-05 12:13:25 +0200

    Update Hybrid Model.py

--- Hybrid Model.py
+++ Hybrid Model.py
@@ -1997,7 +1997,7 @@ def app():
     with col3:
         nightly_bess_revenue = st.number_input("Revenu services système nuit (EUR/nuit)", min_value=0.0, value=0.0, step=10.0)
         soc_steps = st.slider("Nombre de pas de SOC pour l'optimisation", min_value=21, max_value=201, value=51, step=10)
-        initial_soc = st.number_input("SOC initial batterie (MWh)", min_value=0.0, value=batt_energy_mwh, step=1.0)
+        initial_soc = st.number_input("SOC initial batterie (MWh)", min_value=0.0, value=batt_energy_mwh*min_soc_pct, step=1.0)
         final_soc = st.number_input("SOC final cible batterie (MWh)", min_value=0.0, value=60.0, step=1.0)
         min_soc_pct = st.slider("Minimum SOC batterie (%)", 0, 100, 20)
         max_soc_pct = st.slider("Maximum SOC batterie (%)", 0, 100, 90)
