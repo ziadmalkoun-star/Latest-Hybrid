c16dbd811fd1d27ef4b38b837b541aa93acc7389
ziadmalkoun-star <ziad.malkoun@corsicasole.com>
2026-05-06 10:29:24 +0200

    Update Hybrid Model.py

--- Hybrid Model.py
+++ Hybrid Model.py
@@ -2103,6 +2103,8 @@ def app():
         eta_discharge = st.number_input("BESS Discharging Efficiency (%)", min_value=1.0, max_value=100.0, value=95.0, step=0.5) / 100.0
         min_soc_pct = st.slider("BESS Minimum SOC (%)", 0, 100, 20)
         max_soc_pct = st.slider("BESS Maximum SOC (%)", 0, 100, 90)
+        charge_quantile = st.slider("Charge Percentile (%)", 0, 100, 20)
+        discharge_quantile = st.slider("Discharge Percentile (%)", 0, 100, 80)
         initial_soc = st.number_input("BESS BoL SOC (MWh)", min_value=0.0, value=batt_energy_mwh*max_soc_pct/100, step=1.0)
         final_soc = st.number_input("BESS EoL SOC (MWh)", min_value=0.0, value=batt_energy_mwh*min_soc_pct/100, step=1.0)
         bess_capture_rate_pct = st.number_input("BESS Capture Rate (%)", min_value=0.0, max_value=100.0, value=100.0, step=1.0)
@@ -2122,8 +2124,6 @@ def app():
         grid_export_limit_mw = st.number_input("Grid Injection Limit (MW)", min_value=0.0, value=100.0, step=1.0)
         min_spread_arbitrage = st.number_input("Minimum Spread for Arbitrage (EUR/MWh)", min_value=0.0, value=10.0, step=1.0)
         soc_steps = st.slider("SOC Steps for Optimization", min_value=21, max_value=201, value=51, step=10)
-        charge_quantile = st.slider("Charge Percentile (%)", 0, 100, 20)
-        discharge_quantile = st.slider("Discharge Percentile (%)", 0, 100, 80)
 
     bess_degradation_upload = st.file_uploader(
             "BESS Degradation Curve",
